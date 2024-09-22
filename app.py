20b35c4a171edcee14b8c60a1bdef4c7caaf8a61
adrianlfns <adrianlfns@yahoo.com>
2024-09-22 18:25:30 -0400

    connected the edit device

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app = flask.Flask(__name__, static_url_path='/static')
 app.secret_key = '192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bcb1'
 
 @app.get("/")
+@app.get("/home")
 def home():
     '''
     Initial route to land on the home page
@@ -29,15 +30,20 @@ def mydevices():
 def add_edit_device_get(device_id:int=None):
     '''
     entry route for adding/editing devices
+    Note: if device_id ==0 or device_id == NONE, it will be treated as insert.
+          if device_id > 0 it will be treated as edit.
     '''
     data=None
     if device_id:
-        device_id = int(device_id)
-        device = Device_Manager.get_device_by_id(device_id)
-        if not device:
-            raise Exception(f"Device with ID {device_id} not found.")
-        data = device.to_dictionary()
-
+        try:
+            device_id = int(device_id)
+        except ValueError:
+          raise Exception(f"Invalid post data. Expected parameter device_id to be int")
+        if device_id > 0:
+            device = Device_Manager.get_device_by_id(device_id)
+            if not device:
+                raise Exception(f"Device with ID {device_id} not found.")
+            data = device.to_dictionary()
     return flask.render_template('add_edit_device.html',data=data)
 
 @app.post("/remove_device")
@@ -46,10 +52,19 @@ def remove_device():
     POST route for removing a device
     '''
     device_id = flask.request.form.get("device_id",0)  
-    if device_id:       
+
+    if not device_id:
+        raise Exception(f"Invalid post data. expected a parameter named device_id")    
+    try:
         device_id = int(device_id)
-        Device_Manager.delete_device_by_id(device_id)
-        return flask.redirect("/mydevices")
+    except ValueError:
+          raise Exception(f"Invalid post data. Expected parameter device_id to be int")
+    
+    if device_id <=0:
+        raise Exception(f"Invalid post data. Expected parameter device_id to be int > 0")
+
+    Device_Manager.delete_device_by_id(device_id)
+    return flask.redirect("/mydevices")
     
 
 @app.post("/add_edit_device")
